e0f638737fbf3c52ff4edcb82ccc8ff92106dc60
abdool101 <abdoolrazaktasiu101@gmail.com>
2025-12-27 19:28:28 +0100

    Update bot settings for name and owner

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'jabber tech',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botName: "jabber Bot",
+  botOwner: 'king', // jabber
   ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
